1789eb44dafe3774a2619d3029690a23556d794f
theuscompany <168585450+theuscompany@users.noreply.github.com>
2024-11-22 16:43:13 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'historia',
+    'Quem é o atual presidente do Brasil ?',
+    'LULA  #mofome #cadeapicanha??? '
+)
+
+criaCartao(
+    'Quimica',
+    'Qual é a formula da água?',
+    'H2O'
+)
+
+criaCartao(
+    'Carro',
+    'Qual carro normalmente tem uma escada em cima',
+    'UNO'
+)
+
+criaCartao(
+    'História',
+    'Qual é o livro mais vendido do mundo?',
+    'Biblía'
+)
